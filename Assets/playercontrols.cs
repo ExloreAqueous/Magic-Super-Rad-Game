f2a3dad7e32bf1d9c5d656ccd3f972ab043a4ca1
prohacker <57221336+Prohacker00101@users.noreply.github.com>
2019-11-05 09:21:41 -0500

    better

---
+++ Assets/playercontrols.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontrols : MonoBehaviour {
+
+    Rigidbody2D myrb;
+    float xspeed;
+
+    private bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatisGround;
+    public float jumpforce;
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        myrb = this.GetComponent<Rigidbody2D>();
+
+    }
+    void FixedUpdate()
+    {
+        myrb.velocity = new Vector2(xspeed, myrb.velocity.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            xspeed = 5;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            xspeed = -5;
+        }
+        else
+        {
+            xspeed = 0;
+        }
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatisGround);
+        if(isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            myrb.velocity = Vector2.up * jumpforce;
+        }
+    }
+    
+}
