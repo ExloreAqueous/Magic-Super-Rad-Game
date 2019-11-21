17f4b5c7b8e1af729db4fbd10f95ef3b8c11e03d
prohacker <57221336+Prohacker00101@users.noreply.github.com>
2019-11-21 09:50:18 -0500

    fg

---
+++ Assets/HM ALSDFGAISBD/Cowboy/animation/CowBoyController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CowBoyController : MonoBehaviour {
+    public Animator PlayerAnimator;
+    public float RndNumb;
+    // Use this for initialization
+    void Update()
+    {
+        if(Input.GetButton("Horizontal")){
+            PlayerAnimator.SetFloat("RndGen", 1f);
+        }else if(Input.GetButtonUp("Horizontal")){
+            PlayerAnimator.SetFloat("RndGen", 0f);
+        }
+    }
+
+
+}
