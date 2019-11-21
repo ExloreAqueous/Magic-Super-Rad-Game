298b14bfbc423ffaae30785e0c2cc5f1c82c5ffc
prohacker <57221336+Prohacker00101@users.noreply.github.com>
2019-11-21 08:16:27 -0500

    kkk

--- Assets/HM ALSDFGAISBD/demo.cs
+++ Assets/HM ALSDFGAISBD/demo.cs
@@ -2,19 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class demo : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-
-    void FixedUpdate()
-    {
-        
-    }
-    // Update is called once per frame
-    void Update () {
-		
-	}
-}
+//public class demo : MonoBehaviour {
+//    public Animation RRR;
+//    void Update()
+//    {
+//        if (Input.GetKey(KeyCode.LeftArrow))
+//        {
+//            GetComponent<Animator>().SetTrigger(animation(RRR));
+//        }
+//    }
+//}
