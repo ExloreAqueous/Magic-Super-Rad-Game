65ee4a1d05210be3a6ea0c1f98868f59b70cc42a
prohacker <57221336+Prohacker00101@users.noreply.github.com>
2019-12-05 08:52:27 -0500

    Revert "Revert "Revert "idk  im trying to fix it"""
    This reverts commit 4c14eba23b9dad0ea5a1f4786f242fa038c7c533.

--- Assets/HM ALSDFGAISBD/Cowboy/animation/CowBoyController.cs
+++ Assets/HM ALSDFGAISBD/Cowboy/animation/CowBoyController.cs
@@ -9,6 +9,7 @@ public class CowBoyController : MonoBehaviour {
     void Update()
     {
         if(Input.GetButton("Horizontal")){
+<<<<<<< HEAD
             PlayerAnimator.SetFloat("RndGen", 0.5f);
         }else if(Input.GetButtonUp("Horizontal")){
             PlayerAnimator.SetFloat("RndGen", 0f);
@@ -23,6 +24,8 @@ public class CowBoyController : MonoBehaviour {
         }
         if (Input.GetKey(KeyCode.S))
         {
+=======
+>>>>>>> parent of b17d64e... fgf
             PlayerAnimator.SetFloat("RndGen", 1f);
         }else if(Input.GetButtonUp("Horizontal")){
             PlayerAnimator.SetFloat("RndGen", 0f);
