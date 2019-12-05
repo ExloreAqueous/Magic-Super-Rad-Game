da49004a4adf6abc3a4998fb89080634da7a1ff8
prohacker <57221336+Prohacker00101@users.noreply.github.com>
2019-12-05 08:34:52 -0500

    idk  im trying to fix it

--- Assets/HM ALSDFGAISBD/Cowboy/animation/CowBoyController.cs
+++ Assets/HM ALSDFGAISBD/Cowboy/animation/CowBoyController.cs
@@ -9,6 +9,7 @@ public class CowBoyController : MonoBehaviour {
     void Update()
     {
         if(Input.GetButton("Horizontal")){
+<<<<<<< HEAD
             PlayerAnimator.SetFloat("RndGen", 0.5f);
         }else if(Input.GetButtonUp("Horizontal")){
             PlayerAnimator.SetFloat("RndGen", 0f);
@@ -23,10 +24,10 @@ public class CowBoyController : MonoBehaviour {
         }
         if (Input.GetKey(KeyCode.S))
         {
+=======
+>>>>>>> parent of b17d64e... fgf
             PlayerAnimator.SetFloat("RndGen", 1f);
-        }
-        else if (Input.GetButtonUp("Vertical"))
-        {
+        }else if(Input.GetButtonUp("Horizontal")){
             PlayerAnimator.SetFloat("RndGen", 0f);
         }
     }
